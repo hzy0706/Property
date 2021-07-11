f6d844f0c4eac315ec1e725394dc1ac173078452
liuting9 <85324137+liuting9@users.noreply.github.com>
2021-07-12 01:30:40 +0800

    车位信息、楼盘信息

---
+++ property/src/main/java/com/trkj/property/dao/TParkingDao.java
@@ -0,0 +1,21 @@
+package com.trkj.property.dao;
+
+import com.trkj.property.entity.TParking;
+import org.apache.ibatis.annotations.Mapper;
+
+import java.util.List;
+
+@Mapper
+public interface TParkingDao {
+    int deleteByPrimaryKey(Integer parkingId);
+
+    int insert(TParking record);
+
+    int insertTRarKing(TParking tParking);//新增车位信息
+
+    List<TParking> selectTParKing();//显示车位信息
+
+    int updateTParKing(TParking tParking);//修改车位信息
+
+    int updateByPrimaryKey(TParking record);
+}
