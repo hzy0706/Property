335e380fd86a3c6e2184c76136d7336ab6b4862d
dl <1692133787@qq.com>
2021-07-12 16:19:35 +0800

    dxl重新上传上次代码

--- property/src/main/java/com/trkj/property/vo/form/TCostitemQueryForm.java
+++ property/src/main/java/com/trkj/property/vo/form/TCostitemQueryForm.java
@@ -1,51 +1,57 @@
 package com.trkj.property.vo.form;
 
+import java.io.Serializable;
+
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.Data;
 import lombok.ToString;
 
-import java.io.Serializable;
-import java.math.BigDecimal;
-@Data
 @AllArgsConstructor
 @NoArgsConstructor
+@Data
 @ToString
 public class TCostitemQueryForm implements Serializable {
+    private static final long serialVersionUID = -61949355174436553L;
 
-        private Integer ctId;
+    private Integer ctId;
 
-        private Integer residenceId;
+    private Integer residenceId;
 
-        private String ctName;
+    private String ctName;
 
-        private Integer ctChargecycle;
+    private Integer ctChargecycle;
 
-        private BigDecimal ctSubtotal;
+    private Double ctSubtotal;
 
-        private Integer overdue;
+    private Integer overdue;
 
-        private String overduefine;
+    private String overduefine;
+    /**
+     * 关联费项
+     */
+    private String ctRelation;
 
-        private String ctRelation;
+    private String chco;
 
-        private String chco;
+    private String ifprice;
 
-        private String ifprice;
+    private String residenceName;
 
-        private static final long serialVersionUID = 1L;
-        private Integer pageNum;    //当前页
-        private Integer pageSize;   //页大小
+    private String found;
 
-        public Integer getPageNum() {
-            if (pageNum == null)
-                return 1;
-            return pageNum;
-        }
+    private Integer pageNum;    //当前页
+    private Integer pageSize;   //页大小
 
-        public Integer getPageSize() {
-            if (pageSize == null)
-                return 0;
-            return pageSize;
-        }
+    public Integer getPageNum() {
+        if (pageNum == null)
+            return 1;
+        return pageNum;
+    }
+
+    public Integer getPageSize() {
+        if (pageSize == null)
+            return 0;
+        return pageSize;
+    }
 }
