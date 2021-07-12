ba976528b130cc7145db6aca832e5ed8f1e9db33
hzy0706 <85111595+hzy0706@users.noreply.github.com>
2021-07-12 10:26:42 +0800

    修改单元报错

--- property/src/main/java/com/trkj/property/dao/TUnitDao.java
+++ property/src/main/java/com/trkj/property/dao/TUnitDao.java
@@ -1,9 +1,10 @@
 package com.trkj.property.dao;
 
 import com.trkj.property.entity.TUnit;
+import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
-
+@Mapper
 public interface TUnitDao {
 
     TUnit selectByTUnitKey(Integer unitId);
