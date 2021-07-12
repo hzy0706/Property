f7dbcb60a1e7d2e6907876da555773a20ac15b32
liuting9 <85324137+liuting9@users.noreply.github.com>
2021-07-12 15:43:41 +0800

    liutong车位销售修改

--- property/src/main/java/com/trkj/property/service/TCostitemService.java
+++
@@ -1,9 +0,0 @@
-package com.trkj.property.service;
-
-import com.github.pagehelper.PageInfo;
-import com.trkj.property.entity.TCostitem;
-import com.trkj.property.vo.form.TCostitemQueryForm;
-
-public interface TCostitemService {
-    PageInfo<TCostitem> queryAll(TCostitemQueryForm tCostitemQueryForm);
-}
